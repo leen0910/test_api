acf4f6650fdd15c946fa1b376ce855ee992b07a2
leen0910 <16955414@qq.com>
2019-04-16 14:11:31 +0800

    case命名顺序修改

--- common/get_token.py
+++ common/get_token.py
@@ -3,22 +3,29 @@ import requests
 import json
 from common import readconfig
 
-rt=readconfig.ReadConfig()
-API=rt.get_api()
-Prefix=rt.get_prefix()
-account=rt.get_account()
-password=rt.get_pw()
+
 
 class GetToken:
 
+    def setUp(self):
+        rt=readconfig.ReadConfig()
+        self.API=rt.get_api()
+        self.Prefix=rt.get_prefix()
+        self.account=rt.get_account()
+        self.password=rt.get_pw()
+
     def test_token(self):
         """读取配置文件中的帐号后登录"""
-        url='%s%s/users/login'%(API,Prefix)
+        self.setUp()
+        url='%s%s/users/login'%(self.API,self.Prefix)
         header = {
-    'content-type': "application/json",
-    'x-platform': "web",
-    }
-        data = {"account":"%s"%account,"password":"%s"%password} #正确的登录帐号
+            'content-type': "application/json",
+            'x-platform': "web",
+        }
+        data = {
+            "account":"%s"%self.account,
+            "password":"%s"%self.password
+        } #正确的登录帐号
         #将data序列化为json格式数据，传递给data参数
         r = requests.post(url, data=json.dumps(data), headers=header)
         token=r.headers["authorization"]
@@ -26,12 +33,13 @@ class GetToken:
 
     def test_userid(self):
         """读取配置文件中的帐号后登录"""
-        url='%s%s/users/login'%(API,Prefix)
+        self.setUp()
+        url='%s%s/users/login'%(self.API,self.Prefix)
         header = {
-    'content-type': "application/json",
-    'x-platform': "web",
-    }
-        data = {"account":"%s"%account,"password":"%s"%password} #正确的登录帐号
+            'content-type': "application/json",
+            'x-platform': "web",
+        }
+        data = {"account":"%s"%self.account,"password":"%s"%self.password} #正确的登录帐号
         #将data序列化为json格式数据，传递给data参数
         r = requests.post(url, data=json.dumps(data), headers=header)
         loginuser_id=r.json()['data'][0]['id']
